51ceb9668a88c46cf0cf1501c82d0b5df9e85c1b
Wanderson <wandersonguedesfernandes15@gmail.com>
2023-10-10 11:10:28 -0400

    Transformation step added

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from getpass import getuser
 
 def get_all_products(url):
     resp = requests.get(url)
-    print(type(resp.json()))
+    # print(type(resp.json()))
 
     return resp.json(), resp.status_code
 
@@ -14,10 +14,38 @@ if __name__ == '__main__':
 
     api_url = 'https://dummyjson.com/products'
 
-    # TODO Consume a products API to get a json
+    # Extration
     response, code = get_all_products(api_url)
 
     if code != 200:
         print(f'An error {code} as ocurred')
 
     df_product = pd.DataFrame(response['products'])
+
+    # Transformation
+    # Removing culumns
+    df_product = df_product[[
+        'title', 'description', 'price', 'discountPercentage',
+        'rating', 'stock', 'brand', 'category'
+    ]]
+
+    agg_rule = {
+        'price': 'mean',
+        'discountPercentage': 'mean',
+        'rating': 'mean',
+        'stock': 'sum'
+    }
+
+    # Grouping by category
+    df_product_by_category = df_product.groupby(
+        ['category']
+    ).agg(agg_rule).reset_index()
+
+    # Grouping by brand
+    df_product_by_brand = df_product.groupby(
+        ['brand']
+    ).agg(agg_rule).reset_index()
+
+    # Loading (Data Mart)
+    # TODO save the category and brand datas to excel
+
